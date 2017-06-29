2b7c12488d6cca3e0c0d48a181791a7a2b8384bb
shirisha <nalla.shirisha@gmail.com>
2017-06-29 14:30:33 +0530

    Initial commit

---
+++ app/src/main/java/com/example/mlrit/explicitintent/MainActivity1.java
@@ -0,0 +1,24 @@
+package com.example.mlrit.explicitintent;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity1 extends AppCompatActivity {
+Button b1;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        b1=(Button)findViewById(R.id.button);
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i=new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(i);
+            }
+        });
+    }
+}
